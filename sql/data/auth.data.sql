47e1732b48e9184fd0fe5769f9158aca08f0a387
Baptiste <delabordebaptiste8@gmail.com>
2026-03-30 14:40:03 +0200

    data.sql

---
+++ sql/data/auth.data.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (id, email, password, role)
+VALUES (
+    '11111111-1111-1111-1111-111111111111',
+    'photographer1@mail.com',
+    '$2y$10$wH8Q5Lx8zF6S3V5Vn9nQ2e8zWZ9q0Z9QbQp1sYyZzJXz3F8u1z8K2',
+    0
+);
