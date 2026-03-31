73d3387cd04cc7b1e021e73e0850db85ba6a299d
U.N.Owen <lclerc561@gmail.com>
2026-03-31 14:50:17 +0200

    ajout service galerie

---
+++ back/app-galerie/src/application_core/application/ports/api/ServiceGalerieInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace photopro\core\application\ports\api;
+
+interface ServiceGalerieInterface
+{
+    /**
+     * Récupère une galerie par son identifiant et la retourne sous forme de DTO.
+     * * @param string $id L'UUID de la galerie
+     * @return GalerieDTO
+     * @throws \Exception Si la galerie n'est pas trouvée
+     */
+    public function getGalerie(string $id): GalerieDTO;
+
+    /**
+     * Crée une nouvelle galerie (publique ou privée)
+     * * @param string
+     * @param string $title
+     * @param string $layout
+     * @param bool $is_public
+     * @param string|null $description
+     * @param string|null $client_name
+     * @param string|null $client_email
+     * @return GalerieDTO
+     * @throws \InvalidArgumentException
+     */
+    public function createGalerie(
+        string $photographer_id, 
+        string $title, 
+        string $layout, 
+        bool $is_public, 
+        ?string $description = null,
+        ?string $client_name = null,
+        ?string $client_email = null
+    ): GalerieDTO;
+}
