c84c5d2f81175fe7fbc37194a2b1842b1a66489b
Mattéo <matteo.cadet9@etu.univ-lorraine.fr>
2026-03-31 15:06:18 +0200

    nettoyage de guzzle

--- back/gateway-backoffice/src/application/interface/ClientInterface.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-interface ClientInterface {
-    public function get(string $uri): \Psr\Http\Message\ResponseInterface;
-    public function post(string $uri, array $options = []): \Psr\Http\Message\ResponseInterface;
-}
