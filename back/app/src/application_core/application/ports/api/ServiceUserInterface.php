750b7bb661481a2ff86757fb1930f16f9532790f
Quent1540 <dieudonnequent1@gmail.com>
2026-04-02 14:32:27 +0200

    suppression de dossiers redondants

--- back/app/src/application_core/application/ports/api/ServiceUserInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace photopro\core\application\ports\api;
-
-interface ServiceUserInterface{
-
-    public function register(CredentialsDTO $credentials, int $role): ProfileDTO;
-    public function byCredentials(CredentialsDTO $credentials): ?ProfileDTO;
-}
